b66f9d12656c028c013596da5a11bfbb47e1d75b
Muccy <muccymac@gmail.com>
2014-10-30 18:28:13 +0100

    Creating master class

---
+++ Pod/Classes/MUKUserNotificationController.h
@@ -0,0 +1,13 @@
+//
+//  MUKUserNotificationController.h
+//  MUKUserNotificationController
+//
+//  Created by Marco on 30/10/14.
+//  Copyright (c) 2014 Muccy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MUKUserNotificationController : NSObject
+
+@end
---
+++ Pod/Classes/MUKUserNotificationController.m
@@ -0,0 +1,13 @@
+//
+//  MUKUserNotificationController.m
+//  MUKUserNotificationController
+//
+//  Created by Marco on 30/10/14.
+//  Copyright (c) 2014 Muccy. All rights reserved.
+//
+
+#import "MUKUserNotificationController.h"
+
+@implementation MUKUserNotificationController
+
+@end
